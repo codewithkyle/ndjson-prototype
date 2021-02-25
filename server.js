0f29cda5e2d19dcb98a9453e37a2747f949d23dc
Kyle Andrews <codingwithkyle@gmail.com>
2021-02-25 10:39:39 -0500

    Updated demo

--- server.js
+++ server.js
@@ -1,22 +1,5 @@
 const express = require('express');
-const path = require("path");
 const app = express();
 const port = 8080;
-
 app.use(express.static('public'))
-
-const public = path.join(process.cwd(), "public");
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(public, 'index.html'));
-});
-
-app.get('/json', (req, res) => {
-    res.sendFile(path.join(public, "json", 'index.html'));
-});
-
-app.get('/ndjson', (req, res) => {
-    res.sendFile(path.join(public, "ndjson-2", 'index.html'));
-});
-
 app.listen(port);
