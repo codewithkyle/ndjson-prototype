be5a681ddfbfca5d77f1a3d4203ad89577f80f44
Kyle Andrews <codingwithkyle@gmail.com>
2021-12-18 16:29:00 -0500

    Added deployment scripts

--- server.js
+++ server.js
@@ -1,5 +1,33 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const port = 8080;
-app.use(express.static('public'))
+const port = 5005;
 app.listen(port);
+const fs = require("fs");
+const path = require("path");
+
+app.get("/ndjson-3", async (req, res) => {
+    return res.sendFile(path.join(__dirname, "public", "ndjson-3", "index.html"));
+});
+
+app.get("/ndjson-2", async (req, res) => {
+    return res.sendFile(path.join(__dirname, "public", "ndjson-2", "index.html"));
+});
+
+app.get("/ndjson", async (req, res) => {
+    return res.sendFile(path.join(__dirname, "public", "ndjson", "index.html"));
+});
+
+app.get("/json", async (req, res) => {
+    return res.sendFile(path.join(__dirname, "public", "json", "index.html"));
+});
+
+app.get("/", async (req, res) => {
+    return res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("/*", async (req, res) => {
+    let requestedFilePath = path.join(__dirname, "public", req.path);
+    if (fs.existsSync(requestedFilePath)) {
+        return res.sendFile(requestedFilePath);
+    }
+});
